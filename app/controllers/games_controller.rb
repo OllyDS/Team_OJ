ba7b04b044204111f6845894dfdd6c0d7a68c36f
OllyDS <desousa88@gmail.com>
2018-10-22 11:54:03 +0100

    Updated MVC

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,53 @@
 class GamesController < ApplicationController
+  before_action :class_finder, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @games = Game.all
+  end
+
+  def show
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(class_params)
+    if @game.valid?
+      @game.save
+      redirect_to games_path
+    else
+      flash[:errors].errors.full_messages
+      redirect_to new_game_path
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @game.update(class_params).valid?
+      redirect_to game_path(@game)
+    else
+      flash[:errors].errors.full_messages
+      redirect_to edit_game_path
+    end
+  end
+
+  def destroy
+    @game.delete
+    redirect_to games_path
+  end
+
+  private
+
+  def class_finder
+    @game = Game.find(params[:id])
+  end
+
+  def class_params
+    params.require(:game).permit(:name, :bio)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,46 @@
 class UsersController < ApplicationController
+  before_action :class_finder, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(class_finder)
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(class_params).valid?
+      redirect_to user_path(@user)
+    else
+      flash[:errors].errors.full_messages
+      redirect_to edit_user_path
+    end
+  end
+
+  def destroy
+    @user.delete
+    redirect_to users_path
+  end
+
+  private
+
+  def class_finder
+    @user = User.find(params[:id])
+  end
+
+  def class_params
+    params.require(:user).permit(:username, :first_name, :last_name, :email, :encrypted_password, :salt, :img_url)
+  end
+
 end
