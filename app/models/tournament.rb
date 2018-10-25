a79d256649c93e6cde2a7ba7c9878c2cd23bfa3b
joarnie <johnadamarnold@gmail.com>
2018-10-25 09:47:39 +0100

    added placeholders to HOME, added method to TOURNAMENT SHOW

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -2,4 +2,18 @@ class Tournament < ApplicationRecord
   belongs_to :game
   has_many :user_tournaments
   has_many :users, through: :user_tournaments
+
+
+
+  # def t_name_and_date
+  #   self.name + " " + self.date
+  # end
+
+
+  # def t_signed_up?
+  #   @tournament = Tournament.find(params[:id])
+  #   current_user.tournaments.each do
+  # end
+
+
 end
