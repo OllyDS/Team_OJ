9682614bc539b75ef080e5ca7d47a09de58d72fd
joarnie <johnadamarnold@gmail.com>
2018-10-30 19:30:15 +0000

    added American Sams changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
   # Make the current_user method available to views also, not just controllers:
-  helper_method :current_user, :tournament_dates, :tournament_array, :logged_in?
+  helper_method :current_user, :tournament_dates, :tournament_array, :logged_in?, :date_check
   # Define the current_user method:
   def current_user
     # Look up the current user based on user_id in the session cookie:
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
 
   def logged_in?
     !!current_user
@@ -31,6 +32,20 @@ class ApplicationController < ActionController::Base
   def tournament_dates
     @tournaments = Tournament.all.map {|t| t.date }.uniq
   end
+
+  def date_check
+    t = Time.now
+    if tournament_dates == t.strftime("%A %dst %B")
+       "n"
+    elsif tournament_dates == t.strftime("%A %dnd %B")
+       "n"
+    elsif tournament_dates == t.strftime("%A %drd %B")
+       "n"
+    elsif tournament_dates == t.strftime("%A %dth %B")
+       "Today"
+    end
+  end
+
 
   def tournament_array
     @tournaments = Tournament.all
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,19 @@
 class Page < ApplicationRecord
+
+
+
+# def rev
+#   self.reverse
+# end
+
+
+
+
 end
+
+# <% t = Time.now %>
+#  <% if t.strftime("%A %d") +"th "+ t.strftime("%B") == tournament_dates.first %>
+#
+#
+# <%= "Today" %>
+# <% end %>
